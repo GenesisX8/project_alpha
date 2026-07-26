4aa8ddafc132c24ab6249285b75021b417a62e2e
Andrew Clavell <ajclavell17@gmail.com>
2026-07-26 07:13:37 -0700

    feat: Created classes Party and Hero

--- scripts/game_controller.gd
+++ scripts/game_controller.gd
@@ -1,15 +1,45 @@
 extends Node
+class_name GameController
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# Load cards when the game starts
-	var fireball = load("res://cards/card_fireball.tres")
-	print(fireball.name, " costs ", fireball.cost)
+	# load_player_assets()
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
-func use_card(card: Card):
-	print("Using card: ", card.name)
+## TODO: 1. Start of combat
+func start_combat() -> void:
+	## Initialize the combat state and Load assets
+	## Load player assets
+	# load_player_assets()
+
+	## Load enemy assets
+
+	## Set up the initial combat environment
+	
+	## Initialize player and enemy stats
+	
+	## Deal starting hands
+
+	## Determine turn order
+
+	pass
+
+## TODO: 2. Player's turn
+func player_turn() -> void:
+	pass
+
+## TODO: 3. enemy's turn
+func enemy_turn() -> void:
+	pass
+
+## TODO: 4. End of combat
+func end_combat() -> void:
+	pass
+
+func load_player_assets() -> void:
+	pass
--- scripts/player_script.gd
+++ scripts/player_script.gd
@@ -1,17 +1,38 @@
-extends CharacterBody2D
+extends Node
 
 @onready var deck: PlayerDeck = $Deck
+@onready var party: Party = $Party
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("About to print deck...")
+	# Load the player's party
+	print("About to load party...")
 	await get_tree().process_frame
-	print("Calling print_deck() now...")
+	print("Calling load_party() now...")
+	load_party()
+	print("Done calling load_party()")
+	# Finished loading party
+
+	# Load the player's deck
+	print("About to load deck...")
+	await get_tree().process_frame
+	print("Calling load_deck() now...")
 	# Call directly since signal may have already been emitted
-	deck.print_deck()
-	print("Done calling print_deck()")
+	load_deck()
+	print("Done calling load_deck()")
+	# Finished loading deck
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	pass
+
+# Load the player's deck
+func load_deck() -> void:
+	deck.print_deck()
+	pass
+
+# Load the player's party
+func load_party() -> void:
+	party.print_party()
 	pass
